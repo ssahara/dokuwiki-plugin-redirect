acac4ccd4bd8db472c705d3e425858bdeba06920
Satoshi Sahara <sahara.satoshi@gmail.com>
2014-05-21 01:34:11 +0900

    supplementary description for lang_path setting

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -6,6 +6,6 @@
  * @author Satoshi Sahara <sahara.satoshi@gmail.com>
  */
 $lang['showmsg']               = '転送を通知しますか？';
-$lang['conf_path']  = 'リダイレクト定義ファイルの保存場所';
+$lang['conf_path']  = 'リダイレクト定義ファイルの保存場所  (ファーム運用時には この設定によらず、常に DOKU_CONF/redirect.conf となります) ';
   $lang['conf_path_o_0'] = 'lib/plugins/redirect/redirect.conf';
   $lang['conf_path_o_1'] = 'conf/redirect.conf';
